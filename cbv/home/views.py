287b0ea9c7c7716d4e1923e0ff1241b678566992
amiralaeifar <amplusir@gmail.com>
2023-07-19 15:52:33 +0330

    generic View

--- cbv/home/views.py
+++ cbv/home/views.py
@@ -1,8 +1,46 @@
+from typing import Any
+from django import http
 from django.shortcuts import render
 from django.views import View
 
 
 class Home(View):
+    '''
+    django.views.generic.base.View
+    
+    '''
+    #accessible Http method via this view
+    http_method_names = ['get', 'options']
+    
+    
+    def setup(self, request, *args, **kwargs):
+        '''
+            initial setup for the view
+        '''
+        pass
+    
+    
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user:
+            return super().dispatch(request, *args, **kwargs)
+        return AttributeError('you are not authenticated')
+
+
+    def options(self, request, *args, **kwargs):
+        '''
+            showing the request headers.
+            also you can override this, to modify headers.
+        '''
+        response = super().options(request, *args, **kwargs)
+        response.headers['host'] = 'localhost'
+        response.headers['user'] = request.user
+        return response
+    
+    
+    def http_method_not_allowed(self, request, *args, **kwargs):
+        return render(request, 'method_not_allowed.html')
+
     
     def get(self, request):
         return render(request, 'home/home.html')
+    
